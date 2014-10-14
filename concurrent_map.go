88d6e5939627b7d64e52d025e97b537b77ddc140
Roi Lipman <roi@streamrail.com>
2014-10-14 20:25:00 +0300

    restored mutex.

--- concurrent_map.go
+++ concurrent_map.go
@@ -19,8 +19,8 @@ func NewConcurrentMap() *ConcurrentMap {
 
 // Adds an element to the map.
 func (m *ConcurrentMap) Add(key string, value interface{}) {
-	/*m.Lock()
-	defer m.Unlock()*/
+	m.Lock()
+	defer m.Unlock()
 	m.m[key] = value
 }
 
@@ -31,8 +31,8 @@ func (m *ConcurrentMap) Count() int {
 
 // Retrives an element from map under given key.
 func (m *ConcurrentMap) Get(key string) (interface{}, bool) {
-	/*m.RLock()
-	defer m.RUnlock()*/
+	m.RLock()
+	defer m.RUnlock()
 
 	val, err := m.m[key]
 	return val, err
@@ -40,23 +40,23 @@ func (m *ConcurrentMap) Get(key string) (interface{}, bool) {
 
 // Looks up an item under specified key
 func (m *ConcurrentMap) Has(key string) bool {
-	/*m.RLock()
-	defer m.RUnlock()*/
+	m.RLock()
+	defer m.RUnlock()
 	_, ok := m.m[key]
 	return ok
 }
 
 // Removes an element from the map.
 func (m *ConcurrentMap) Remove(key string) {
-	/*m.Lock()
-	defer m.Unlock()*/
+	m.Lock()
+	defer m.Unlock()
 	delete(m.m, key)
 }
 
 // Clears map by constructing a new one.
 func (m *ConcurrentMap) Clear() {
-	/*m.Lock()
-	defer m.Unlock()*/
+	m.Lock()
+	defer m.Unlock()
 	m.m = make(map[string]interface{})
 }
 
@@ -76,8 +76,8 @@ func (m *ConcurrentMap) Iter() <-chan Tuple {
 	//ch := make(chan interface{})
 	ch := make(chan Tuple)
 	go func() {
-		/*m.RLock()
-		defer m.RUnlock()*/
+		m.RLock()
+		defer m.RUnlock()
 		for key, val := range m.m {
 			ch <- Tuple{key, val}
 		}
