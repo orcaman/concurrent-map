6b864e4d3f16817c5fca806ae12abb67323c9c99
orcaman <or@skyline.ai>
2021-05-01 21:23:45 +0300

    calc key len once

--- concurrent_map_bench_test.go
+++ concurrent_map_bench_test.go
@@ -6,304 +6,66 @@ import (
 	"testing"
 )
 
-func BenchmarkItems(b *testing.B) {
+var K = 100000
+
+func BenchmarkItemsCMap(b *testing.B) {
 	m := New()
 
-	// Insert 100 elements.
-	for i := 0; i < 10000; i++ {
+	// Insert K elements.
+	for i := 0; i < K; i++ {
 		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
 	}
-	for i := 0; i < b.N; i++ {
-		m.Items()
+	for i := 0; i < K; i++ {
+		m.Get(strconv.Itoa(i))
 	}
 }
 
-func BenchmarkMarshalJson(b *testing.B) {
+func BenchmarkItemsSyncMap(b *testing.B) {
+	m := sync.Map{}
+
+	// Insert 100000 elements.
+	for i := 0; i < K; i++ {
+		m.Store(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+	}
+	for i := 0; i < K; i++ {
+		m.Load(strconv.Itoa(i))
+	}
+}
+
+func BenchmarkItemsCMapConcurrent(b *testing.B) {
 	m := New()
+	wg := &sync.WaitGroup{}
 
-	// Insert 100 elements.
-	for i := 0; i < 10000; i++ {
-		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+	for i := 0; i < K; i++ {
+		wg.Add(1)
+		go func(i int) {
+			m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+			wg.Done()
+		}(i)
 	}
-	for i := 0; i < b.N; i++ {
-		_, err := m.MarshalJSON()
-		if err != nil {
-			b.FailNow()
-		}
-	}
-}
 
-func BenchmarkStrconv(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		strconv.Itoa(i)
+	wg.Wait()
+
+	for i := 0; i < K; i++ {
+		m.Get(strconv.Itoa(i))
 	}
 }
 
-func BenchmarkSingleInsertAbsent(b *testing.B) {
-	m := New()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		m.Set(strconv.Itoa(i), "value")
-	}
-}
+func BenchmarkItemsSyncMapConcurrent(b *testing.B) {
+	m := sync.Map{}
+	wg := &sync.WaitGroup{}
 
-func BenchmarkSingleInsertAbsentSyncMap(b *testing.B) {
-	var m sync.Map
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		m.Store(strconv.Itoa(i), "value")
+	for i := 0; i < K; i++ {
+		wg.Add(1)
+		go func(i int) {
+			m.Store(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+			wg.Done()
+		}(i)
 	}
-}
 
-func BenchmarkSingleInsertPresent(b *testing.B) {
-	m := New()
-	m.Set("key", "value")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		m.Set("key", "value")
-	}
-}
+	wg.Wait()
 
-func BenchmarkSingleInsertPresentSyncMap(b *testing.B) {
-	var m sync.Map
-	m.Store("key", "value")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		m.Store("key", "value")
-	}
-}
-
-func benchmarkMultiInsertDifferent(b *testing.B) {
-	m := New()
-	finished := make(chan struct{}, b.N)
-	_, set := GetSet(m, finished)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go set(strconv.Itoa(i), "value")
-	}
-	for i := 0; i < b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiInsertDifferentSyncMap(b *testing.B) {
-	var m sync.Map
-	finished := make(chan struct{}, b.N)
-	_, set := GetSetSyncMap(&m, finished)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go set(strconv.Itoa(i), "value")
-	}
-	for i := 0; i < b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiInsertDifferent_1_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiInsertDifferent, b, 1)
-}
-func BenchmarkMultiInsertDifferent_16_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiInsertDifferent, b, 16)
-}
-func BenchmarkMultiInsertDifferent_32_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiInsertDifferent, b, 32)
-}
-func BenchmarkMultiInsertDifferent_256_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetDifferent, b, 256)
-}
-
-func BenchmarkMultiInsertSame(b *testing.B) {
-	m := New()
-	finished := make(chan struct{}, b.N)
-	_, set := GetSet(m, finished)
-	m.Set("key", "value")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go set("key", "value")
-	}
-	for i := 0; i < b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiInsertSameSyncMap(b *testing.B) {
-	var m sync.Map
-	finished := make(chan struct{}, b.N)
-	_, set := GetSetSyncMap(&m, finished)
-	m.Store("key", "value")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go set("key", "value")
-	}
-	for i := 0; i < b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiGetSame(b *testing.B) {
-	m := New()
-	finished := make(chan struct{}, b.N)
-	get, _ := GetSet(m, finished)
-	m.Set("key", "value")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go get("key", "value")
-	}
-	for i := 0; i < b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiGetSameSyncMap(b *testing.B) {
-	var m sync.Map
-	finished := make(chan struct{}, b.N)
-	get, _ := GetSetSyncMap(&m, finished)
-	m.Store("key", "value")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go get("key", "value")
-	}
-	for i := 0; i < b.N; i++ {
-		<-finished
-	}
-}
-
-func benchmarkMultiGetSetDifferent(b *testing.B) {
-	m := New()
-	finished := make(chan struct{}, 2*b.N)
-	get, set := GetSet(m, finished)
-	m.Set("-1", "value")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go set(strconv.Itoa(i-1), "value")
-		go get(strconv.Itoa(i), "value")
-	}
-	for i := 0; i < 2*b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiGetSetDifferentSyncMap(b *testing.B) {
-	var m sync.Map
-	finished := make(chan struct{}, 2*b.N)
-	get, set := GetSetSyncMap(&m, finished)
-	m.Store("-1", "value")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go set(strconv.Itoa(i-1), "value")
-		go get(strconv.Itoa(i), "value")
-	}
-	for i := 0; i < 2*b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiGetSetDifferent_1_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetDifferent, b, 1)
-}
-func BenchmarkMultiGetSetDifferent_16_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetDifferent, b, 16)
-}
-func BenchmarkMultiGetSetDifferent_32_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetDifferent, b, 32)
-}
-func BenchmarkMultiGetSetDifferent_256_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetDifferent, b, 256)
-}
-
-func benchmarkMultiGetSetBlock(b *testing.B) {
-	m := New()
-	finished := make(chan struct{}, 2*b.N)
-	get, set := GetSet(m, finished)
-	for i := 0; i < b.N; i++ {
-		m.Set(strconv.Itoa(i%100), "value")
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go set(strconv.Itoa(i%100), "value")
-		go get(strconv.Itoa(i%100), "value")
-	}
-	for i := 0; i < 2*b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiGetSetBlockSyncMap(b *testing.B) {
-	var m sync.Map
-	finished := make(chan struct{}, 2*b.N)
-	get, set := GetSetSyncMap(&m, finished)
-	for i := 0; i < b.N; i++ {
-		m.Store(strconv.Itoa(i%100), "value")
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		go set(strconv.Itoa(i%100), "value")
-		go get(strconv.Itoa(i%100), "value")
-	}
-	for i := 0; i < 2*b.N; i++ {
-		<-finished
-	}
-}
-
-func BenchmarkMultiGetSetBlock_1_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetBlock, b, 1)
-}
-func BenchmarkMultiGetSetBlock_16_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetBlock, b, 16)
-}
-func BenchmarkMultiGetSetBlock_32_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetBlock, b, 32)
-}
-func BenchmarkMultiGetSetBlock_256_Shard(b *testing.B) {
-	runWithShards(benchmarkMultiGetSetBlock, b, 256)
-}
-
-func GetSet(m ConcurrentMap, finished chan struct{}) (set func(key, value string), get func(key, value string)) {
-	return func(key, value string) {
-			for i := 0; i < 10; i++ {
-				m.Get(key)
-			}
-			finished <- struct{}{}
-		}, func(key, value string) {
-			for i := 0; i < 10; i++ {
-				m.Set(key, value)
-			}
-			finished <- struct{}{}
-		}
-}
-
-func GetSetSyncMap(m *sync.Map, finished chan struct{}) (get func(key, value string), set func(key, value string)) {
-	get = func(key, value string) {
-		for i := 0; i < 10; i++ {
-			m.Load(key)
-		}
-		finished <- struct{}{}
-	}
-	set = func(key, value string) {
-		for i := 0; i < 10; i++ {
-			m.Store(key, value)
-		}
-		finished <- struct{}{}
-	}
-	return
-}
-
-func runWithShards(bench func(b *testing.B), b *testing.B, shardsCount int) {
-	oldShardsCount := SHARD_COUNT
-	SHARD_COUNT = shardsCount
-	bench(b)
-	SHARD_COUNT = oldShardsCount
-}
-
-func BenchmarkKeys(b *testing.B) {
-	m := New()
-
-	// Insert 100 elements.
-	for i := 0; i < 10000; i++ {
-		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
-	}
-	for i := 0; i < b.N; i++ {
-		m.Keys()
+	for i := 0; i < K; i++ {
+		m.Load(strconv.Itoa(i))
 	}
 }
